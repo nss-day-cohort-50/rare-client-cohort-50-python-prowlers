34f07f851fb1393c602e180411463b1e34019ca7
Travis Hall <t.hall89d@gmail.com>
2021-10-27 11:27:07 -0400

    forgot to save

--- src/components/posts/CurrentUserPosts.js
+++ src/components/posts/CurrentUserPosts.js
@@ -17,7 +17,21 @@ export const CurrentUserPosts = () => {
     <>
       <div className="post_feed">
         {userPosts?.map((post) => {
-          return <div className="post_container"></div>;
+          return (
+            <div className="post_container">
+              <div className="top">
+                <div className="post_title">{post?.title}</div>
+                <div className="post_date">{post?.publication_date}</div>
+              </div>
+              <div className="center">
+                <div className="post_image">{post?.image_url}</div>
+              </div>
+              <div className="bottom">
+                <div className="post_author"></div>
+                <div className="post_reaction"></div>
+              </div>
+            </div>
+          );
         })}
       </div>
     </>
